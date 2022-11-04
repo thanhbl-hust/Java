1cab69cbed09bb95259d35bb915f6faa92aa964c
thanhbl-hust <lamthanhbui02@gmail.com>
2022-11-04 14:24:08 +0700

    Basic Java

---
+++ java fundementals/7.string.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String[] args) {
+		String s = "Codelearn";
+
+
+
+		System.out.print(s.length());
+        //length of a string in java;
+	}
+}
