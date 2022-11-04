aa908320e90cbd5e6127fcbb8906b2d404415765
thanhbl-hust <lamthanhbui02@gmail.com>
2022-11-04 14:32:22 +0700

    Basic Java

--- java fundementals/6.2.array2D.java
+++ java fundementals/6.2.array2D.java
@@ -6,13 +6,11 @@ public class Array {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int[][] arr = new int[n][m];
-
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				arr[i][j] = sc.nextInt();
 			}
 		}
-
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				System.out.println("arr[" + i + "][" + j + "] = " + arr[i][j]);
